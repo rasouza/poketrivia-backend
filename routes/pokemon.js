988340611fff95465c5e6a22da3ee0be202d8f5e
Rodrigo Souza <alves.wm@gmail.com>
2019-01-17 10:43:03 -0200

    implements answer endpoint

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Jimp = require('jimp');
 
+var answers = {
+  1: 2
+};
+
 router.get('/', function(req, res) {
   axios
     .get('https://pokeapi.co/api/v2/pokemon/125')
@@ -12,6 +16,11 @@ router.get('/', function(req, res) {
         })
         .catch(error => console.log(error));
     });
+});
+
+router.post('/question/:id', function(req, res) {
+  result = answers[req.params.id] === req.body.answer;
+  res.json({ answer: result });
 });
 
 module.exports = router;
